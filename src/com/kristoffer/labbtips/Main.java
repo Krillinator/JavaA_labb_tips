802bccae4dbe264c252480df03722273b433f39a
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2023-09-26 11:32:13 +0200

    clean slate

--- src/com/kristoffer/labbtips/Main.java
+++ src/com/kristoffer/labbtips/Main.java
@@ -1,39 +1,7 @@
 package com.kristoffer.labbtips;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-
-        /* TODO - Multiple Dice
-        *   Dynamic & DRY code
-        *   Nested For-loop
-        * */
-
-        Scanner sc = new Scanner(System.in);
-
-        int rounds = 5;
-
-        System.out.println("How many dice?");
-        int dice = sc.nextInt();
-        System.out.println("You picked: " + dice);
-
-        // Rounds
-        for (int i = 0; i < rounds; i++) {
-            System.out.println("Round: " + i);
-
-            // Iterate Dice
-            for (int j = 0; j < dice; j++) {
-                Random random = new Random();
-
-                int score = random.nextInt(6) + 1;
-                System.out.println("Throwing Die " + score);
-            }
-
-        }
-
-
 
     }
 }
